bc6905eb9f34abdc3cb4b90eeffdec0eb5b0fcb2
Charles Matthew M. Salut <charlesmatthew.salut@cit.edu>
2024-05-09 19:02:16 +0800

    recommit

--- src/pages/queue.js
+++ src/pages/queue.js
@@ -1,82 +1,100 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import "../css/queue.css";
 
-export default function Queue(){
-    const [queueList, setQueueList] = useState([]);
-    const [calledNumb, setCalledNumb] = useState([]);
+function Queue() {
+  const [queue, setQueue] = useState([]);
+  const [currentCustomer, setCurrentCustomer] = useState(null);
 
-    useEffect(() => {
-        axios.get("http://localhost:8080/queue/getAllQueue")
-            .then(response => {
-                const nonPopped = response.data.filter(item => item.isPopped === 0);
-                setQueueList(nonPopped);
-                console.log(response)
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/queue/getAllQueue")
+      .then((response) => {
+        const nonPopped = response.data.filter((item) => item.isPopped === 0);
+        setQueue(nonPopped);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
 
-        axios.get("http://localhost:8080/queue/calledNumb")
-            .then(responda => {
-                // console.log(responda);
-                setCalledNumb(responda.data);
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            })
-    }, []);
+    axios
+      .get("http://localhost:8080/queue/calledNumb")
+      .then((response) => {
+        setCurrentCustomer(response.data);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []);
 
-    const handleCallNum = () => {
-        axios.get("http://localhost:8080/queue/popQueue")
-            .then(responseCallNum => {
-                console.log(responseCallNum)
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            })
+  const handleCallNumber = () => {
+    axios
+      .get("http://localhost:8080/queue/popQueue")
+      .then((response) => {
+        setCurrentCustomer(response.data);
+        setQueue(queue.filter((item) => item.id !== response.data.id));
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+
+  const handleSkipCustomer = () => {
+    if (currentCustomer) {
+      axios
+        .post("http://localhost:8080/queue/skipQueue", currentCustomer)
+        .then(() => {
+          setCurrentCustomer(null);
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
     }
+  };
 
-    return(
-        <div className="queue-main">
-            <div className="queue-area">
-                <div className='queue-table-wrapper'>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Number</th>
-                                <th>User</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {queueList.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.id}</td>
-                                    <td>{item.firstname}</td>
-                                </tr>        
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+  return (
+    <div className="queue-system">
+      <div className="queue-list">
+        {queue.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Number</th>
+                <th>User</th>
+              </tr>
+            </thead>
+            <tbody>
+              {queue.map((customer) => (
+                <tr key={customer.id}>
+                  <td>{customer.id}</td>
+                  <td>{customer.firstname}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No customers in queue.</p>
+        )}
+      </div>
+      <div className="now-serving">
+        <h2>NOW SERVING</h2>
+        {currentCustomer ? (
+          <div>
+            <p className="number">{currentCustomer.id}</p>
+            <p className="name">{currentCustomer.firstname}</p>
+            <div className="button-container">
+              <button onClick={handleCallNumber}>Call Next</button>
+              <button className="skip" onClick={handleSkipCustomer}>
+                Skip
+              </button>
             </div>
-
-            <div className="functions-area">
-                <div className='queue-number-box'>
-                    {calledNumb ? (
-                        <>
-                            <p className='queue-called-number'>{calledNumb.id}</p>
-                            <p className='queue-called-name'>{calledNumb.firstname}</p>
-                        </>
-                    ):(
-                        <>
-                            <p className='queue-called-number'>0</p>
-                            <p className='queue-called-name'>none</p>
-                        </>
-                    )}
-                </div>
-
-                <button onClick={handleCallNum}>Call Number</button>
-            </div>
-        </div>
-    );
+          </div>
+        ) : (
+          <p>No one is currently being served.</p>
+        )}
+      </div>
+    </div>
+  );
 }
+
+export default Queue;
